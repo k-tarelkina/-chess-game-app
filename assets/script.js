76f06d64d07b1a2da35fb2469a745e6b04479479
Kateryna Tarelkina <kt.tarelkina@gmail.com>
2024-02-13 16:04:11 +0100

    Add chess board and basic JS script

--- assets/script.js
+++ assets/script.js
@@ -1,50 +1,81 @@
-function getPlotConfig(pointsCoordinates) {
-  mapped = [];
+window.addEventListener("load", () => {
+  chessBoard = document.getElementById("chess-board");
 
-  for (let i = 0; i < pointsCoordinates.length - 1; i++) {
-    mapped.push({
-      x: pointsCoordinates[i][0],
-      y: pointsCoordinates[i][1],
-    });
+  if (chessBoard) {
+    for (let i = 7; i >= 0; i--) {
+      for (let j = 0; j < 8; j++) {
+        const cell = new Cell(i, j);
+        chessBoard.appendChild(cell.element);
+      }
+    }
   }
-  return {
-    type: "scatter",
-    data: {
-      datasets: [
-        {
-          label: "Scatter Dataset",
-          data: mapped,
-          backgroundColor: "rgb(255, 99, 132)",
-        },
-      ],
-    },
-    options: {
-      scales: {
-        x: {
-          type: "linear",
-          position: "bottom",
-        },
-      },
-    },
-  };
+});
+
+class Cell {
+  piece = null;
+  element = null;
+
+  constructor(x, y) {
+    this.element = document.createElement("div");
+    this.element.addEventListener("click", () => console.log(x, y));
+    if ((x % 2 == 0 && y % 2 == 0) || (x % 2 == 1 && y % 2 == 1)) {
+      this.element.classList.add("white");
+    } else {
+      this.element.classList.add("black");
+    }
+    console.log(this.element);
+  }
+
+  putPiece(piece) {
+    const prevPiece = this.piece;
+    this.piece = piece;
+    return prevPiece;
+  }
 }
 
-function plotPoints(pointsCoordinates) {
-  const ctx = document.getElementById("chart");
+class Chessboard {
+  clearCell(x, y) {}
 
-  new Chart(ctx, getPlotConfig(pointsCoordinates));
+  putPiece(x, y, pice) {}
 }
 
-function setInput(input) {
-  const fReader = new FileReader();
+class ChessPiece {
+  x;
+  y;
 
-  fReader.readAsDataURL(input.files[0]);
+  ChessFigure(x, y) {
+    this.x = x;
+    this.y = y;
+  }
 
-  fReader.onloadend = (event) => {
-    const image = document.getElementById("inputImage");
-    image.src = event.target.result;
-    result = window.pywebview.api
-      .getPointsCoordinates(event.target.result)
-      .then(plotPoints);
-  };
+  moveTo(x, y) {
+    if (!this.canMoveTo(x, y)) {
+      console.log("Cannot move to these coordinates");
+    } else {
+      this.x = x;
+      this.y = y;
+    }
+  }
+
+  canMoveTo(x, y) {
+    return false;
+  }
+
+  select() {}
+}
+
+/*
+King - Moves one square in any direction.
+Queen - Moves any number of squares diagonally, horizontally, or vertically.
+Rook - Moves any number of squares horizontally or vertically.
+Bishop - Moves any number of squares diagonally.
+Knight - Moves in an ‘L-shape,’ two squares in a straight direction, and then one square perpendicular to that.
+Pawn - Moves one square forward, but on its first move, it can move two squares forward. It captures diagonally one square forward.
+
+*/
+
+class King extends ChessPiece {
+  canMoveTo(x, y) {
+    return Math.abs(this.x - x) > 1 || Math.abs(this.y - y) > 1;
+  }
 }
