cdef1e4f816bef3450d0ac3adf0adac5fcde48e0
Kateryna Tarelkina <kt.tarelkina@gmail.com>
2024-02-15 21:58:57 +0100

    Add possibility to select

--- assets/script.js
+++ assets/script.js
@@ -1,51 +1,143 @@
 window.addEventListener("load", () => {
-  chessBoard = document.getElementById("chess-board");
-
-  if (chessBoard) {
-    for (let i = 7; i >= 0; i--) {
-      for (let j = 0; j < 8; j++) {
-        const cell = new Cell(i, j);
-        chessBoard.appendChild(cell.element);
-      }
-    }
-  }
+  const chessBoard = new Chessboard();
+  chessBoard.initializeBoard();
+  chessBoard.initializePieces();
 });
+
+const COLOR = {
+  Black: "Black",
+  White: "White",
+};
 
 class Cell {
   piece = null;
   element = null;
+  x = 0;
+  y = 0;
 
-  constructor(x, y) {
+  constructor(x, y, chessBoard) {
+    this.x = x;
+    this.y = y;
+    this.chessBoard = chessBoard;
     this.element = document.createElement("div");
-    this.element.addEventListener("click", () => console.log(x, y));
-    if ((x % 2 == 0 && y % 2 == 0) || (x % 2 == 1 && y % 2 == 1)) {
+
+    this.element.addEventListener("click", () => {
+      this.select();
+    });
+
+    if ((x % 2 == 1 && y % 2 == 0) || (x % 2 == 0 && y % 2 == 1)) {
       this.element.classList.add("white");
     } else {
       this.element.classList.add("black");
     }
-    console.log(this.element);
   }
 
   putPiece(piece) {
     const prevPiece = this.piece;
     this.piece = piece;
+    this.element.innerText = this.piece.getName();
     return prevPiece;
+  }
+
+  select() {
+    this.chessBoard.clearSelectedCells();
+    if (this.piece) {
+      this.element.classList.add("selected");
+      this.chessBoard.setSelectedCell(this.x, this.y);
+    }
+  }
+
+  clear() {
+    this.element.innerText = "";
+    // remove piece
   }
 }
 
 class Chessboard {
-  clearCell(x, y) {}
+  cells = [[], [], [], [], [], [], [], []];
 
-  putPiece(x, y, pice) {}
+  peviouslySelectedCells = [];
+
+  setBlackPiecesUser(username) {}
+  setWhitePiecesUser(username) {}
+
+  startGame() {}
+
+  clearSelectedCells() {
+    for (const cell of this.peviouslySelectedCells) {
+      cell.element.classList.remove("selected");
+    }
+    this.peviouslySelectedCells = [];
+  }
+
+  setSelectedCell(x, y) {
+    this.peviouslySelectedCells.push(this.cells[x][y]);
+  }
+
+  clearCell(x, y) {
+    this.cells[x][y].clear();
+  }
+
+  putPiece(x, y, piece) {
+    console.log(this.cells);
+    this.cells[x][y].putPiece(piece);
+  }
+
+  disableColor() {}
+  enableColor() {}
+
+  addDeadPiece(piece) {
+    // Render new piece
+  }
+
+  initializeBoard() {
+    const chessBoard = document.getElementById("chess-board");
+
+    if (chessBoard) {
+      for (let i = 7; i >= 0; i--) {
+        for (let j = 0; j < 8; j++) {
+          const cell = new Cell(i, j, this);
+          this.cells[i].push(cell);
+          chessBoard.appendChild(cell.element);
+        }
+      }
+    }
+  }
+
+  initializePieces() {
+    /*
+King - Moves one square in any direction.
+Queen - Moves any number of squares diagonally, horizontally, or vertically.
+Rook - Moves any number of squares horizontally or vertically.
+Bishop - Moves any number of squares diagonally.
+Knight - Moves in an ‘L-shape,’ two squares in a straight direction, and then one square perpendicular to that.
+Pawn - Moves one square forward, but on its first move, it can move two squares forward. It captures diagonally one square forward.
+
+*/
+    const piecesConfig = [
+      {
+        create: () => King(),
+        config: {
+          x: 0,
+          y: 0,
+        },
+      },
+    ];
+
+    const king = new King(0, 0, this);
+  }
 }
 
 class ChessPiece {
   x;
   y;
 
-  ChessFigure(x, y) {
+  constructor(x, y, chessBoard) {
     this.x = x;
     this.y = y;
+    this.chessBoard = chessBoard;
+    this.image = "image";
+    this.chessBoard.putPiece(x, y, this);
   }
 
   moveTo(x, y) {
@@ -54,14 +146,23 @@ class ChessPiece {
     } else {
       this.x = x;
       this.y = y;
+      this.chessBoard.clearCell(x, y);
+      const prevPiece = this.chessBoard.putPiece(x, y, this);
+      prevPiece?.die();
     }
+  }
+
+  die() {
+    this.chessBoard.addDeadPiece(this);
   }
 
   canMoveTo(x, y) {
     return false;
   }
 
-  select() {}
+  getName() {
+    return "Chess piece";
+  }
 }
 
 /*
@@ -77,5 +178,9 @@ Pawn - Moves one square forward, but on its first move, it can move two squares 
 class King extends ChessPiece {
   canMoveTo(x, y) {
     return Math.abs(this.x - x) > 1 || Math.abs(this.y - y) > 1;
+  }
+
+  getName() {
+    return "King";
   }
 }
