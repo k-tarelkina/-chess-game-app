b6ebb189da5f33cd04fbe58904250b871a12bac6
Kateryna Tarelkina <kt.tarelkina@gmail.com>
2024-02-24 11:49:44 +0100

    Add Queen paths

--- src/Queen.cpp
+++ src/Queen.cpp
@@ -9,5 +9,70 @@ std::string Queen::getName()
 
 std::vector<Coordinates> Queen::getPossiblePaths()
 {
-  return std::vector<Coordinates>{{1, 1}, {5, 4}};
+  std::vector<Coordinates> paths;
+  auto directions = getDirections();
+
+  for (auto direction : directions)
+  {
+    std::vector<Coordinates> currentPath;
+
+    for (int i = 1; i < 8; i++)
+      currentPath.push_back(direction(x_, y_, i));
+
+    currentPath = prunePath(currentPath);
+    paths.insert(paths.end(), currentPath.begin(), currentPath.end());
+  }
+
+  return paths;
+}
+
+std::vector<Coordinates> Queen::prunePath(std::vector<Coordinates> path)
+{
+  std::vector<Coordinates> prunedPath;
+  for (auto p : path)
+  {
+    if (p.first >= 8 || p.second >= 8 || p.first < 0 || p.second < 0)
+    {
+      break;
+    }
+
+    if (isPieceOfSameColor(p.first, p.second))
+    {
+      break;
+    }
+    prunedPath.push_back(p);
+
+    if (isPieceOfOppositeColor(p.first, p.second))
+    {
+      break;
+    }
+  }
+  return prunedPath;
+}
+
+std::vector<std::function<Coordinates(int, int, int)>> Queen::getDirections()
+{
+  std::vector<std::function<Coordinates(int, int, int)>> directions;
+
+  // Horizontal-vertical directions
+  directions.push_back([](int x, int y, int i)
+                       { return std::make_pair(x + i, y); });
+  directions.push_back([](int x, int y, int i)
+                       { return std::make_pair(x - i, y); });
+  directions.push_back([](int x, int y, int i)
+                       { return std::make_pair(x, y + i); });
+  directions.push_back([](int x, int y, int i)
+                       { return std::make_pair(x, y - i); });
+
+  // Diagonal directions
+  directions.push_back([](int x, int y, int i)
+                       { return std::make_pair(x - i, y - i); });
+  directions.push_back([](int x, int y, int i)
+                       { return std::make_pair(x - i, y + i); });
+  directions.push_back([](int x, int y, int i)
+                       { return std::make_pair(x + i, y - i); });
+  directions.push_back([](int x, int y, int i)
+                       { return std::make_pair(x + i, y + i); });
+
+  return directions;
 }
--- src/Queen.h
+++ src/Queen.h
@@ -3,6 +3,7 @@
 #include "ChessPiece.h"
 #include <vector>
 #include <string>
+#include <functional>
 
 class Queen : public ChessPiece
 {
@@ -12,4 +13,12 @@ public:
 
   std::string getName() override;
   std::vector<Coordinates> getPossiblePaths() override;
+
+private:
+  std::vector<Coordinates> prunePath(std::vector<Coordinates> path);
+
+  // Helper function to calculate direction from a given point (x, y) by
+  // incrementing it by a value of i
+  // (int, int, int) corresponds to (x, y, i)
+  std::vector<std::function<Coordinates(int, int, int)>> getDirections();
 };
