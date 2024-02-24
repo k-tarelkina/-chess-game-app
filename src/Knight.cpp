1fe1297f454743e04e59ff873ed78c69c4a3f97b
Kateryna Tarelkina <kt.tarelkina@gmail.com>
2024-02-24 12:09:05 +0100

    Add Knight paths

--- src/Knight.cpp
+++ src/Knight.cpp
@@ -9,5 +9,22 @@ std::string Knight::getName()
 
 std::vector<Coordinates> Knight::getPossiblePaths()
 {
-  return std::vector<Coordinates>{{1, 1}, {5, 4}};
+  std::vector<Coordinates> possiblePaths = {
+      {x_ + 1, y_ + 2},
+      {x_ + 2, y_ + 1},
+      {x_ - 1, y_ + 2},
+      {x_ - 2, y_ + 1},
+      {x_ + 1, y_ - 2},
+      {x_ + 2, y_ - 1},
+      {x_ - 1, y_ - 2},
+      {x_ - 2, y_ - 1}};
+
+  std::vector<Coordinates> correctPaths;
+
+  std::copy_if(possiblePaths.begin(), possiblePaths.end(), std::back_inserter(correctPaths),
+               [this](Coordinates p)
+               { return p.first < 8 && p.second < 8 && p.first >= 0 && p.second >= 0 &&
+                        !this->isPieceOfSameColor(p.first, p.second); });
+
+  return correctPaths;
 }
